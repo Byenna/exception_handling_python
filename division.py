c6a1ae1e21bfd41e416db76474d3804e63a3f870
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-06 17:26:23 +0200

    added exceptions

--- division.py
+++ division.py
@@ -1,13 +1,17 @@
 def divide_numbers():
+    try:
     # Write a program that asks the user to input two numbers and performs division on them. 
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
 
-    result = num1 / num2
+        result = num1 / num2
 
-    print(f"The result is: {result}.")
+        print(f"The result is: {result}.")
 
-  
+    except ZeroDivisionError:
+        print("Error: Cannot devide by 0")
+    except ValueError:
+        print("Please enter numeric values.")
 
-  
-    divide_numbers()
+
+divide_numbers()
