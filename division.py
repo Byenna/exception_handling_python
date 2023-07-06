22e9c3778d3e02e8871376fd4bee246aa1d55df8
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-06 17:15:25 +0200

    created function

--- division.py
+++ division.py
@@ -1,4 +1,13 @@
 def divide_numbers():
-    
+    # Write a program that asks the user to input two numbers and performs division on them. 
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
+    result = num1 / num2
+
+    print(f"The result is: {result}.")
+
+  
+
+  
     divide_numbers()
